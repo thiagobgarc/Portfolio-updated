8404b4fccfaa262240edc9affbe080d0d79062d9
Thiago <thiagobuenogarcia1@gmail.com>
2024-03-06 09:49:28 -0500

    fixing some more styling for mobile view

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -15,7 +15,7 @@ const Icons = () => {
   return (
     <motion.div className='flex items-center justify-center mt-[27rem] p-0 gap-4' initial={{ y: 0, opacity: 0 }} animate={{ y:15, opacity: 1 }} transition={{ duration: 0.9 }}>
       {iconVariants.map((icon, index) => (
-        <a key={index} href={icon.link} target="_blank" rel="noopener noreferrer" className={`icon-image hover:scale-150 duration-300 w-[10%] mt-[-5rem] lg:mt-[-2rem] sm:mt-[-2rem] lg:top-[-2rem] sm:top-[-2rem] lg:w-[3.5%] sm:w-[7%] mb-[2rem] lg:mb-0 sm:mb-0 download=${true}`}>
+        <a key={index} href={icon.link} target="_blank" rel="noopener noreferrer" className={`icon-image hover:scale-150 duration-300 w-[10%] mt-[-7rem] lg:mt-[-2rem] sm:mt-[-2rem] lg:top-[-2rem] sm:top-[-2rem] lg:w-[3.5%] sm:w-[7%] mb-[2rem] lg:mb-0 sm:mb-0 download=${true}`}>
           <img src={icon.icon} alt={`Icon ${index}`} />
         </a>
       ))}
